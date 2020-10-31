b9c150a1517195b8c2b5b4d2a3d5bc5e8c745c08
Subhradeep Bhattacharya <37349689+iamsubhro@users.noreply.github.com>
2020-10-31 09:43:36 -0700

    Update QueueUsingTwoStacks.java
    Updated Code..

--- QueueUsingTwoStacks.java
+++ QueueUsingTwoStacks.java
@@ -1,112 +1,47 @@
+package datastructures;
+
 import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+public class Queueusingtwostacks {
 
-public class QueueUsingTwoStacks {
+	    public static void main(String[] args) {
+	        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
 
-    public static void main(String[] args) {  
-        
-        //Creating two Stacks
-        arrayStack s1 = new arrayStack();
-        arrayStack s2 = new arrayStack();
-        
-        //Creating object of Scanner Class
-        Scanner sc = new Scanner(System.in);
-        
-        int q,type,x;
-        
-        //number of queries
-        q = sc.nextInt();
-        
-        //iterating for each queries
-        for(int k=0;k<q;k++){
-            type = sc.nextInt();
+	        Scanner sc= new Scanner(System.in);
+	        Stack<Integer> front= new Stack<>();
+	        Stack<Integer> rear= new Stack<>();
 
-            //Enqueue
-            if(type==1){
-                x = sc.nextInt();
-                //Push to stack 1
-                s1.push(x);
-            }
-            //Dequeue
-            if(type==2){
-                //moving all elements from stack1 to stack2
-                if(s2.empty()){
-                    while(!s1.empty()){
-                        s2.push(s1.peek());
-                        s1.pop();
-                    }
-                }
-                //poping first element of stack2 i.e. front of queue
-                s2.pop();
-            }
-            //Printing Last element
-            if(type==3){
-                //moving all elements from stack1 to stack2
-                if(s2.empty()){
-                    while(!s1.empty()){
-                        s2.push(s1.peek());
-                        s1.pop();
-                    }
-                }
-                //Printing First element of stack2 is that front of queue
-                System.out.println(s2.peek());
-            }
-        }
-    }
-    
-}
+	        int n=sc.nextInt();
 
-class arrayStack{
+	        while(n-- > 0){
+	            
+	            int t=sc.nextInt();
+	            
+	            if(t==1){
 
-    int arr[];
-    int top, size, len;
+	                int x=sc.nextInt();
+	                rear.push(x);    
+	            }
+	            else{
 
-    /*  Constructor for arrayStack */
+	                if(front.isEmpty()){
 
-    public arrayStack(){
-        size = 100000; // array max size
-        len = 0;
-        arr = new int[size];
-        top = -1;
-    }
+	                    while(!rear.isEmpty()){
+	                        front.push(rear.peek());
+	                        rear.pop();
+	                    }
+	                }
 
-    /*  Function to check if stack is empty */
-    public boolean empty(){
+	                if(!front.isEmpty()){
 
-        return top == -1;
+	                    if(t == 2)
+	                    front.pop();
 
-    }
-
-    /*  Function to check the top element of the stack */
-    public int peek(){
-
-        return arr[top];
-
-    }
-
-    /*  Function to add an element to the stack */
-    public void push(int i)
-
-    {
-
-        if(top + 1 < size )
-
-            arr[++top] = i;
-
-        len++ ;
-
-    }
-
-    /*  Function to delete an element from the stack */
-    public int pop(){
-
-        len-- ;
-
-        return arr[top--]; 
-
-    }    
+	                    if(t == 3)
+	                    System.out.println(front.peek());
+	                }
+	            }
+	        }
+	    }
 
 }
